5c2b81f122f866cd179c2b727344a2cce8c8f921
himanshu <118502619+himaenshuu@users.noreply.github.com>
2025-08-08 01:24:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Docker, can i fuck you !");
+  res.end("Hello from Docker!");
 });
 
 server.listen(PORT, () => {
